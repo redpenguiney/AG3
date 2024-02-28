bcba2333d39bd1aed58bb82e74400088d22d77e5
redpenguiney <119638232+redpenguiney@users.noreply.github.com>
2024-02-28 07:09:27 -0800

    forgot a thing

--- src/graphics/framebuffer.cpp
+++ src/graphics/framebuffer.cpp
@@ -31,10 +31,12 @@ Framebuffer::~Framebuffer() {
 
 // TODO: apparently sometimes you want to use an argument besides GL_FRAMEBUFFER?
 void Framebuffer::StartDrawingWith() {
+    glViewport()
     glBindFramebuffer(GL_FRAMEBUFFER, glFramebufferId);
 }
 
 // TODO: again, apparently sometimes you want to use an argument besides GL_FRAMEBUFFER?
 void Framebuffer::StopDrawingWith() {
+    glViewport(GLint x, GLint y, GLsizei width, GLsizei height)
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
